77e6405118142873040f8dc6f4d4d22f82f32dd8
seangibbz <sean.d.gibbons@gmail.com>
2016-12-15 05:10:25 -0700

    added base command launcher

--- wiicodebot.py
+++ wiicodebot.py
@@ -7,7 +7,7 @@ urllib3.disable_warnings() # Disables SSL console warning
 full_dict = {} # Creates base dictionary
     # Entries will take the form {'user1':{'game1a':'game1a-friendcode', 'game1b':'game1b-friendcode'}, 'user2':{'game2a':'game2a-friendcode', 'game1b':'game1b-friendcode'}}
 
-game_list = {'WII1':"Wii Friend Code 1", 'WII2':"Wii Friend Code 2", 'WII3':"Wii Friend Code 3", 'MKW':"Mario Kart Wii (Wiimmfi)", 'ACCF':"Animal Crossing: City Folk (Wiimmfi)", 'SSBB':"Super Smash Bros. Brawl (Wiimmfi)"] # list of supported games
+game_list = {'WII1':"Wii Friend Code 1", 'WII2':"Wii Friend Code 2", 'WII3':"Wii Friend Code 3", 'MKW':"Mario Kart Wii (Wiimmfi)", 'ACCF':"Animal Crossing: City Folk (Wiimmfi)", 'SSBB':"Super Smash Bros. Brawl (Wiimmfi)"} # list of supported games
 
 tokenfile = open('token.txt') # Private bot token file
 token = tokenfile.read()[:-1]
@@ -18,12 +18,13 @@ command_list = ['setCode','getAllGames','getAllUsers','getUsersOf','getCode','ge
 
 @client.event
 async def on_message(message):
-    author = message.author
+    author = str(message.author)
     content = message.content
-    print(str(message) + ':', content)
+    print(message)
+    print(author + ':', content)
     if content[0] == command_prefix:
-        if content[1:] in command_list:
-            pass
+        if content[1:].split(" ")[0] in command_list:
+            execute_command(content[1:],author)
         else: # If the user called the bot but with an invalid command
             print("Error: Invalid command", content[1:])
 
@@ -34,6 +35,38 @@ async def on_ready(): # When the bot launches
     print('Client ID:', client.user.id)
     print('------')
     print(client)
+
+def execute_command(command_entered,command_author): # command syntax checker / launcher
+    user_args = command_entered.split(" ")
+    base_command = user_args[0]
+    if base_command == 'setCode':
+        if len(user_args) == 3:
+            setCode(command_author, user_args[1], user_args[2])
+        else:
+            print("Error: Incorrect number of arguments")
+    elif base_command == 'getAllGames':
+        getAllGames()
+    elif base_command == 'getAllUsers':
+        getAllUsers()
+    elif base_command == 'getUsersOf':
+        if len(user_args) == 2:
+            getUsersOf(user_args[1])
+        else:
+            print("Error: Incorrect number of arguments")
+    elif base_command == 'getCode':
+        if len(user_args) == 3:
+            getCode(user_args[1], user_args[2])
+        else:
+            print("Error: Incorrect number of arguments")
+    elif base_command == 'getUsersAndCodesOf':
+        if len(user_args) == 2:
+            getUsersAndCodesOf(user_args[1])
+        else:
+            print("Error: Incorrect number of arguments")
+    elif base_command == 'help':
+        help()
+    elif base_command == 'terminate':
+        terminate(command_author)
 
 def setCode(username, game, code): # Lets the user set his/her code for a specific game
     pass
